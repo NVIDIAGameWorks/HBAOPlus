8eb5da4732e88e900171b90867a4f2e602ad7f32
lbavoil <lbavoil@nvidia.com>
2016-04-19 10:26:26 +0200

    Update Linux build script

--- build/platforms/gcc/build_linux.sh
+++ build/platforms/gcc/build_linux.sh
@@ -11,4 +11,4 @@ for f in $SrcDir/*cpp; do echo "#include "\"$f\" >> All.cpp; done
 
 g++ -c -O3 -DSUPPORT_GL=1 -DLINUX=1 -I$IncludeDir -fPIC -o All.o -std=c++11 All.cpp
 
-g++ -shared -o $OutDir/GFSDK_SSAO.x86_64-linux-gnu.so All.o
+g++ -shared -o $OutDir/GFSDK_SSAO_GL.x86_64-linux-gnu.so All.o
