7db4c579de1cbc73882cdbf98dfb996223307181
lbavoil <lbavoil@nvidia.com>
2016-04-19 09:50:18 +0200

    Add Linux build

---
+++ build/platforms/gcc/build_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OutDir=../../../lib
+SrcDir=../../../src
+IncludeDir=../../../include
+
+mkdir $OutDir 2>/dev/null
+
+rm -f All.cpp
+for f in $SrcDir/*cpp; do echo "#include "\"$f\" >> All.cpp; done
+
+g++ -c -O3 -DSUPPORT_GL=1 -DLINUX=1 -I$IncludeDir -fPIC -o All.o -std=c++11 All.cpp
+
+g++ -shared -o $OutDir/GFSDK_SSAO.x86_64-linux-gnu.so All.o
